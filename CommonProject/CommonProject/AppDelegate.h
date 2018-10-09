68508e4e02d8066c0d6feb7bdc9a13ed8f39a6e4
ios <>
2018-10-09 14:42:57 +0800

    项目初始化

---
+++ CommonProject/CommonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonProject
+//
+//  Created by ios on 2018/10/9.
+//  Copyright © 2018 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
