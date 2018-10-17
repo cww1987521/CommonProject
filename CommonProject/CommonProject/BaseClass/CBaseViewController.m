782e7827dfedd145738d4544d2b5e082ce3ee344
ios <>
2018-10-17 10:04:48 +0800

    添加第一版basecontroller

---
+++ CommonProject/CommonProject/BaseClass/CBaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  CBaseViewController.m
+//  CommonProject
+//
+//  Created by ios on 2018/10/12.
+//  Copyright © 2018 ios. All rights reserved.
+//
+
+#import "CBaseViewController.h"
+
+@interface CBaseViewController ()
+
+@end
+
+@implementation CBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
